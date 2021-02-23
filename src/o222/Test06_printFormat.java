f1546019752ef14e26cfa01feef56f67868c09ca
seongsik-choi <rlawlsdk1225@gmail.com>
2021-02-23 15:46:06 +0900

    JAVA

--- src/o222/Test06_printFormat.java
+++ src/o222/Test06_printFormat.java
@@ -12,6 +12,21 @@ public class Test06_printFormat {
 	   System.out.prinf("출력서식", 값);
 	 */
 		System.out.printf("정수: %d의 실수형 값은 : %f \n", 100, 100.0);
-		System.out.printf("%c는 문자 %s는 문자열 %d는 정수, %f는 실수", 'A', "AB", 2 ,2.4);
-	}
+		System.out.printf("%c는 문자 %s는 문자열 %d는 정수, %f는 실수 \n", 'A', "AB", 2 ,2.4);
+		
+		System.out.printf("#%5d\n", 123);     //#  123  		5칸확보후 123
+		System.out.printf("#%-5d#\n", 123);	  //#123  #			5칸확보 앞에서부터 123
+		System.out.printf("#%05d#\n", 123);	  //#00123# 		5칸확보 입력값보다 공간남을시 0으로
+		
+		System.out.printf("#%6.2f#\n", 7.8);	//#  7.80#		6칸확보 소수 둘째자리까지
+		System.out.printf("#%-6.2f#\n", 7.8);	//#7.80  # 		6칸확보 소수 둘째자리까지(앞에서부터)
+		System.out.printf("#%.2f#\n", 7.8);		//#7.80#		확보 no 소수 둘째자리까지
+		
+		System.out.printf("#%5c#\n", 'R');		//#    R#		5칸확보 
+		System.out.printf("#%-5c#\n", 'R');  	//#R    #		5칸확보(앞에서부터)
+		
+		System.out.printf("#%8s#\n", "HAPPY");	//#   HAPPY#	8칸확보 
+		System.out.printf("#%-8s#\n", "HAPPY");	//#HAPPY   #	8칸확보(앞에서부터)
+		
+		System.out.println(String.format("%.4f", 10/3.0));	// "3.333" String형으로 형 변환
 }
---
+++ src/o223/Test01_method.java
@@ -0,0 +1,16 @@
+package o223;
+public class Test01_method {
+	public static void main(String[] args) {
+	/*
+	 * 0223 Test01_method
+	 * - 함수 : Function
+	 * 1) 일정한 기능을 가지고 있는 것
+	 * 2) 작성법 : 리턴형 함수명 () {  함수의 기능  }
+	 * 3) 식별자 : project, package, class, method, variable name 등 특성에 맞게 의미부여
+	 * 4) "test"  : String constants
+	 * 	   test   : variable	
+	 *     test() : method
+	 * 5) User defined method(사용자 정의 함수)   
+	 */
+	}
+}
